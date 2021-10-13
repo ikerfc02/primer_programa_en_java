5cca5bc2f2898fd7fd3d3e65eca50362839330e4
ikerfc <ikerfc02@gmail.com>
2021-10-13 14:10:41 +0200

    Añadido cantante

--- PruebaJava.java
+++ PruebaJava.java
@@ -2,9 +2,9 @@ public class PruebaJava {
 
 	public static void main(String args[]) {
 
-	System.out.println("Íker Fernández Carpintero");
 	System.out.println("León, 2002");
 	System.out.println("León, Windows 10");
+	System.out.println("Blake");
 
 	}
 }
